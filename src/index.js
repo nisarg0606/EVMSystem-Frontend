03f9394d17a517906ab821f4b56f8006da9e68ee
Nisarg Shah <nisarg0606@gmail.com>
2024-04-24 18:37:23 -0400

    trying to deploy

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
@@ -6,7 +5,7 @@ import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import "assets/vendor/nucleo/css/nucleo.css";
 import "assets/vendor/font-awesome/css/font-awesome.min.css";
 import "assets/scss/argon-design-system-react.scss?v1.1.0";
-import 'tailwindcss/tailwind.css';
+import "tailwindcss/tailwind.css";
 
 import Index from "views/Index.js";
 import Landing from "views/examples/Landing.js";
@@ -22,17 +21,17 @@ import Home from "views/examples/Home";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/">
     <Routes>
       <Route path="/" exact element={<Index />} />
       <Route path="/landing" exact element={<Landing />} />
       <Route path="/login" exact element={<Login />} />
       <Route path="/profile" exact element={<Profile />} />
       <Route path="/register" exact element={<Register />} />
-      <Route path="/venue" exact element={<Venue/>}/>
-      <Route path="/activity" exact element={<Activity/>}/>
-      <Route path="/peoples" exact element={<People/>}/>
-      <Route path="/home" exact element={<Home/>}/>
+      <Route path="/venue" exact element={<Venue />} />
+      <Route path="/activity" exact element={<Activity />} />
+      <Route path="/peoples" exact element={<People />} />
+      <Route path="/home" exact element={<Home />} />
       {/* <Route path="/test-page" exact element={<Test/>}/> */}
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
