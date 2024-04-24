fb5eadedaed2f558855fa99e23eda8b734c3790c
Nisarg Shah <nisarg0606@gmail.com>
2024-04-24 16:46:08 -0400

    setup and some adjustments

--- src/views/examples/Test.js
+++ src/views/examples/Test.js
@@ -1,40 +1,43 @@
 import React, { useEffect, useState } from 'react';
+import Cookies from 'js-cookie';
 
 const BASE_URL = "http://localhost:5000/";
 
 function Test() {
   const [venues, setVenues] = useState([]);
-  
+
   useEffect(() => {
     const fetchVenues = async () => {
       try {
-        const response = await fetch(`${BASE_URL}venues`, {
+        const token = Cookies.get('token');
+
+        const response = await fetch(`${BASE_URL}venues/`, {
           headers: {
             "Content-Type": "application/json",
-            "Authorization": `Bearer ${localStorage.getItem('token')}`,
+            "Authorization": `Bearer ${token}`, 
+            "credentials": "same-origin"
           }
         });
+
         if (!response.ok) {
           throw new Error('Failed to fetch venues');
         }
-        const venuesData = await response.json();
-        setVenues(venuesData);
+
+        const responseData = await response.json();
+        // Extracting venues from the response object
+        const venuesArray = Object.values(responseData.venues);
+        setVenues(venuesArray);
       } catch (error) {
         console.error(error);
       }
     };
 
     fetchVenues();
-  }, []); 
+  }, []);
 
   return (
     <div>
-      <h1>Venues</h1>
-      <ul>
-        {venues.map(venue => (
-          <li key={venue._id}>{venue.name}</li>
-        ))}
-      </ul>
+    
     </div>
   );
 }
